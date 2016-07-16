e32187d0ee483e9be1adf3cfe44373c41924d60c
giblackjack <stephen.carlson123@gmail.com>
2016-07-16 12:35:13 -0500

    Java application to manage duplicate pictures on individual machines
    Program is still in early stages of development and testing.  Initial
    project for me to get back into programming.

---
+++ src/duplicatepicturefinder/GUI.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duplicatepicturefinder;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author STIVY
+ */
+public class GUI {
+    Frame F1;
+    FileDialog chooser;
+    WindowAdapter myWindowAdapter;
+    
+    public GUI() {
+        //construct needed objects
+        myWindowAdapter = new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent we) {
+                super.windowClosing(we); //To change body of generated methods, choose Tools | Templates.
+                System.exit(0);
+            }
+        };
+    }
+    //setup main window and show window
+    public void show(){
+        this.F1 = new Frame("test frame");
+        F1.add(new Label("Hello World!"));
+        F1.addWindowListener(myWindowAdapter);
+        F1.pack();
+        F1.setVisible(true);
+        
+        chooser = new FileDialog(F1,"Choose a Folder",FileDialog.LOAD);
+        chooser.setVisible(true);
+        String dir = chooser.getDirectory();
+        F1.add(new Label(dir));
+        F1.pack();
+    }
+    
+    
+}
